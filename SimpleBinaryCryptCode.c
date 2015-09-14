e193207892babcaa27f74a8b060c223c6dfc80fc
David Gao <fovecifer@gmail.com>
2015-09-14 19:30:01 +0800

    add case

--- SimpleBinaryCryptCode.c
+++ SimpleBinaryCryptCode.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+int table[2][2][2][2][2][2][2];
+
 int main() {
-    printf("Hello World!\n");
+    table[0][0][0][1][1][0][1] = 0;
+    table[0][0][1][1][0][0][1] = 1;
+    table[0][0][1][0][0][1][1] = 2;
+    table[0][1][1][1][1][0][1] = 3;
+    table[0][1][0][0][0][1][1] = 4;
+    table[0][1][1][0][0][0][1] = 5;
+    table[0][1][0][1][1][1][1] = 6;
+    table[0][1][1][1][0][1][1] = 7;
+    table[0][1][1][0][1][1][1] = 8;
+    table[0][0][0][1][0][1][1] = 9;
+
+    int cases;
+    int i, j, k;
+    scanf("%d", &cases);
+
+    for (i = 0; i < cases; i++) {
+        int N, M;
+        int data[100][100];
+        scanf("%d %d", &N, &M);
+        for (j = 0; j < N; j++) {
+            char str[102];
+            scanf("%s", &str);
+            for (k = 0; k < M; k++) {
+                data[j][k] = str[k] - 48;
+            }
+        }
+    }
 }

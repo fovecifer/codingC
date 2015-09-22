c86e4e4767189f261f14833c9bd1126ed2fd89b5
David Gao <fovecifer@gmail.com>
2015-09-22 10:32:33 +0800

    finish CryptographicCode.c

--- CryptographicCode.c
+++ CryptographicCode.c
@@ -4,6 +4,7 @@ int table[4][4][4];
 
 int MAX_N = 2000;
 int MAX_M = 500;
+
 /*
  * 
  */
@@ -19,18 +20,18 @@ void getBinary(char hex, int *result) {
 }
 
 int getSum(int *start) {
-    int c = (start[0] + start[2] + start[4] + start[6]) * 3 + start[1] 
+    int c = (start[0] + start[2] + start[4] + start[6]) * 3 + start[1]
             + start[3] + start[5] + start[7];
-    if(c % 10 == 0) {
-        return start[0] + start[2] + start[4] + start[6] + start[1] 
-            + start[3] + start[5] + start[7];
-    }else {
+    if (c % 10 == 0) {
+        return start[0] + start[2] + start[4] + start[6] + start[1]
+                + start[3] + start[5] + start[7];
+    } else {
         return 0;
     }
 }
 
 int main(int argc, char** argv) {
-    
+
     table[1][0][0] = 0;
     table[1][1][0] = 1;
     table[0][1][1] = 2;
@@ -41,48 +42,57 @@ int main(int argc, char** argv) {
     table[2][0][1] = 7;
     table[1][0][2] = 8;
     table[0][0][1] = 9;
-    
+
     int case_num;
     int i, j, k;
     scanf("%d", &case_num);
-    
-    for(i = 0; i < case_num; i++) {
+
+    for (i = 0; i < case_num; i++) {
         int N, M;
         scanf("%d %d", &N, &M);
         char data[N][M];
         /* full file data */
-        for(j = 0; j < N; j++) {
+        for (j = 0; j < N; j++) {
             char str[MAX_M];
             scanf("%s", str);
-            for(k = 0; k < M; k++) {
+            for (k = 0; k < M; k++) {
                 data[j][k] = str[k];
             }
         }
-        
+
         int sum = 0;
         int code[8];
         /* find all hex code */
-        for(j = 0; j < N; j++) {
-            for(k = (M - 1); k >= 0; k--) {
-                if(data[j][k] != '0' && (data[j - 1][k] != data[j][k])) {
+        for (j = 0; j < N; j++) {
+            for (k = (M - 1); k >= 0; k--) {
+                if (data[j][k] != '0' && (data[j - 1][k] != data[j][k])) {
                     int bit = 0;
                     int tmp, l;
                     getBinary(data[j][k], &tmp);
-                    
-                    while (k >= 13) {
-                        while (1 == 1) {
-                            if ((tmp & 0x1) == 0) {
-                                tmp = tmp >> 1;
-                                bit++;
-                            } else {
-                                break;
-                            }
-                        }
 
+                    while (k >= 13) {
+                        if(data[j - 1][k] == data[j][k]) {
+                            break;
+                        }
                         for (l = 7; l >= 0; l--) {
-                            int x = 0; 
-                            int y = 0; 
-                            int z = 0; 
+                            while (1 == 1) {
+                                if ((tmp & 0x1) == 0) {
+                                    if (bit != 3) {
+                                        tmp = tmp >> 1;
+                                        bit++;
+                                    } else {
+                                        getBinary(data[j][--k], &tmp);
+                                        bit = 0;
+                                    }
+                                } else {
+                                    break;
+                                }
+                            }
+
+
+                            int x = 0;
+                            int y = 0;
+                            int z = 0;
                             int min;
                             do {
                                 z++;
@@ -138,11 +148,27 @@ int main(int argc, char** argv) {
                                 sum = getSum(code) + sum;
                             }
                         }
+                        while (1 == 1) {
+                            if (k < 13) {
+                                break;
+                            }
+                            if ((tmp & 0x1) == 0) {
+                                if (bit != 3) {
+                                    tmp = tmp >> 1;
+                                    bit++;
+                                } else {
+                                    getBinary(data[j][--k], &tmp);
+                                    bit = 0;
+                                }
+                            } else {
+                                break;
+                            }
+                        }
                     }
                 }
             }
         }
-        
+
         printf("%d %d\n", (i + 1), sum);
     }
 
